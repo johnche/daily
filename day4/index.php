fc67b3194e7ebf75093fc48f3245ef4550dea373
johnche <john20chen@gmail.com>
2016-10-05 01:58:04 +0200

    day 4 added

---
+++ day4/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.4/semantic.min.css">
+<style type="text/css">
+body > .grid{
+	height:100%;
+}
+.column{
+	max-width:450px;
+}
+</style>
+
+
+<div class="ui middle aligned center aligned grid">
+
+<div class="column">
+
+	<form class="ui small form" method="post">
+		<div class="ui stacked segment">
+
+			<div class="field">
+				<div class="ui input">
+					<input type="text" name="username" placeholder="user">
+				</div>
+			</div>
+
+			<div class="field">
+				<div class="ui input">
+					<input type="text" name="password" placeholder="pass">
+				</div>
+			</div>
+
+			<input type="submit" value="red button" class="ui fluid small red button">
+
+		</div>
+	</form>
+
+	<div class="ui message">
+<?php
+if ($_POST["username"] == null && $_POST["password"] == null){
+	echo "press red button";
+}
+$credentials = explode("\n", file_get_contents("credentials.txt"));
+$username = $credentials[0];
+$password = $credentials[1];
+
+if ($_POST["username"] == $username && $_POST["password"] == $password){
+	echo "Good job.";
+} else if ($_POST["username"] != null || $_POST["password"] != null) { 
+	echo "Wrong"; 
+}
+?>
+</div>
+
+</div>
+</div>
+
